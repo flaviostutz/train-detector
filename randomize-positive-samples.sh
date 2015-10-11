8d0a810ab62f3da98880ac1fa69580d227ce2bbd
Stutz <flaviostutz@gmail.com>
2015-10-11 04:07:50 +0000

    Created randomize-positive-samples for rotating plates on positives samples

---
+++ randomize-positive-samples.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$1" == "" ]; then
+  echo "Usage: $0 [directory containing images]"
+  exit 1
+fi
+
+rm positive/* -R
+
+for f in $1/*
+do
+   filename=$(basename $f)
+   fname=${filename%.*}
+   posdir=positive/$fname
+   echo "Processing $fname..."
+   opencv_createsamples -img $f -bg negative/negative.txt -info $posdir/positive.txt -num 5 -maxxangle 0.15 -maxyangle 0.15 -maxzangle 0.2 -w 125 -h 46 -bgcolor 255 -bgthresh 0
+   awk '{print "'"$fname"'/" $0}' $posdir/positive.txt >> positive/positive.txt
+done
+
+nr_imgs=$(wc -l positive/positive.txt | awk '{print $1}')
+cd positive
+opencv_createsamples -info positive.txt -bg ../negative/negative.txt -vec vecfile.vec -num $nr_imgs -w 125 -h 46
